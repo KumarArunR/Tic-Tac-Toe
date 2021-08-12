06e1a32190eec542d2819cbd9778489124efe22b
arundjo0109@gmail.com <arundjo0109@gmail.com>
2021-08-12 12:20:58 +0530

    final commit

---
+++ src/App/index.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react'
+import Algo from './algo'
+import Result from './result'
+import { Box } from './styled'
+import UserModal from './userModal'
+
+const App = () => {
+    let initial = [[null, null, null], [null, null, null], [null, null, null]]
+    const [user, setUser] = useState({ 'p1': 'player 1', 'p2': 'player 2' })
+    const [val, setVal] = useState(true)
+    const [win, setWin] = useState("")
+    const [mat, setMat] = useState(initial)
+    const [modalShow, setModalShow] = useState(false)
+    const [userModalShow, setUserModalShow] = useState(true)
+
+    useEffect(() => {
+        async function algocall() {
+            let res = await Algo(mat)
+            if (res.res === "p1") {
+                setWin("p1")
+                setModalShow(true)
+                console.log("result:", mat)
+            }
+            if (res.res === "p2") {
+                setWin("p2")
+                setModalShow(true)
+            }
+            if (res.res === "draw") {
+                setWin("draw")
+                setModalShow(true)
+            }
+        }
+        algocall()
+    }, [mat])
+
+    function XO(e, i, j) {
+        e.target.setAttribute("style", "pointer-events: none;");
+        if (val === true)
+            e.target.innerHTML = "<p>X</p>"
+        else
+            e.target.innerHTML = "<p>O</p>"
+        let newMat = [...mat]
+        newMat[i][j] = val
+        setMat(newMat)
+        setVal(!val)
+    }
+
+    return (
+        <>
+            <UserModal setShow={setUserModalShow} show={userModalShow} user={user} setUser={setUser} />
+            <h1 className="text-center mt-5">Tic Tak Toe</h1>
+            <div className="container justify-content-center mt-5 d-grid">
+                <div className="row mt-5">
+                    <Box className="col" pos="top" side="left" onClick={e => XO(e, 0, 0)}></Box>
+                    <Box className="col" pos="top" onClick={e => XO(e, 0, 1)}></Box>
+                    <Box className="col" pos="top" side="right" onClick={e => XO(e, 0, 2)}></Box>
+                </div>
+                <div className="row">
+                    <Box className="col" side="left" onClick={e => XO(e, 1, 0)}></Box>
+                    <Box className="col" onClick={e => XO(e, 1, 1)}></Box>
+                    <Box className="col" side="right" onClick={e => XO(e, 1, 2)}></Box>
+                </div>
+                <div className="row">
+                    <Box className="col" side="left" pos="bottom" onClick={e => XO(e, 2, 0)}></Box>
+                    <Box className="col" pos="bottom" onClick={e => XO(e, 2, 1)}></Box>
+                    <Box className="col" side="right" pos="bottom" onClick={e => XO(e, 2, 2)}></Box>
+                </div>
+            </div>
+            <div className="container text-center border border-2 mt-5">
+                {
+                    val ? <h4 className="text-success">{user.p1} Turn</h4> : <h4 className="text-danger">{user.p2} Turn</h4>
+                }
+            </div>
+            <Result show={modalShow} user={user} win={win} handleClose={() => setModalShow(false)} />
+        </>
+    )
+}
+
+export default App;
---
+++ src/App/result.jsx
@@ -0,0 +1,36 @@
+import { Modal, Button } from 'react-bootstrap'
+const Result = ({ show, win, handleClose, user }) => {
+    return (
+        <Modal
+            show={show}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Winner!!!!
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {
+                    win === "draw" ? <h6 className="text-danger">Match tie</h6> :
+                        <h6 className="text-success">{
+                            win === "p1" ? user.p1 : user.p2
+                        } won the game</h6>
+                }
+
+
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={() => window.location = "/"}>
+                    Play again
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
+export default Result;
